d3c840bde02fb85b3493d29304e27d41e9ee7d92
VITOR <VITOR@VITORC>
2020-01-31 16:05:25 -0500

    commit all

--- Program.cs
+++ Program.cs
@@ -23,82 +23,88 @@ namespace PROGRAM1
             {
                 // This variable will gather the user input
                 string input = Console.ReadLine();
-                /* 
-                   This variable will be used to store sales
-                   after the input variable is parsed to decimal
-                 */
-                // This variable will be used to calculate the bonus
+                
+                // This variable will be used to calculate the grade 
                 decimal grade = decimal.Parse(input);
-                /* 
-                    This IF/ELSE IF/ELSE statement is used to conditionally
-                    test the users input. Various options are available
-                    depending on the amount entered by the user. Then, insde of each IF/ELSE IF/ELSE
-                    there is a switch conditional statement that displays a message depending
-                    on the number of years experience in sales entered by the user. 
-                 */
-                if (grade >= 98)
+                                
+                if ((grade >= 98) && (grade <=100))
                 {
-
+                    //grade A+
                     Console.WriteLine("Your Letter grade will be A+");
-                    // Now get the number of years the user has been in sales
+                    
 
 
                 }
                 else if ((grade >= 92) && (grade <= 97))
                 {
+                    //grade A
                     Console.WriteLine("Your Letter grade will be A");
                 }
                 else if ((grade >= 90) && (grade <= 91))
                 {
+                    //grade A-
                     Console.WriteLine("Your Letter grade will be A-");
                 }
                 else if ((grade >= 88) && (grade <= 89))
                 {
+                    //grade B+
                     Console.WriteLine("Your Letter grade will be B+");
                 }
                 else if ((grade >= 82) && (grade <= 87))
                 {
+                    //grade B
                     Console.WriteLine("Your Letter grade will be B");
                 }
                 else if ((grade >= 80) && (grade <= 81))
                 {
+                    //grade B-
                     Console.WriteLine("Your Letter grade will be B-");
                 }
                 else if ((grade >= 78) && (grade <= 79))
                 {
+                    //grade C+
                     Console.WriteLine("Your Letter grade will be C+");
                 }
                 else if ((grade >= 72) && (grade <= 77))
                 {
+                    //grade C
                     Console.WriteLine("Your Letter grade will be C");
                 }
                 else if ((grade >= 70) && (grade <= 71))
                 {
+                    //grade C-
                     Console.WriteLine("Your Letter grade will be C-");
                 }
                 else if ((grade >= 68) && (grade <= 69))
                 {
+                    //grade D+
                     Console.WriteLine("Your Letter grade will be D+");
                 }
                 else if ((grade >= 62) && (grade <= 69))
                 {
+                    //grade D
                     Console.WriteLine("Your Letter grade will be D");
                 }
                 else if ((grade >= 60) && (grade <= 61))
                 {
+                    //grade D-
                     Console.WriteLine("Your Letter grade will be D-");
                 }
-                else if (grade < 60)
+                else if ((grade < 60) && (grade >= 0))
                 {
+                    //grade F
                     Console.WriteLine("Your Letter grade will be F");
+                }
+                else
+                {
+                    // error message display
+                    Console.WriteLine("Enter number between 1 and 100");
                 }
             } // end of try
             catch
             {
-                Console.WriteLine("Please use a decimal data type for your sales amount next time...");
-                Console.WriteLine("--- OR ---");
-                Console.WriteLine("Please use an integer data type for your number of years in sales next time ...");
-                Console.WriteLine("Press any key to exit the program and try again...");
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
                 Console.ReadKey(true);
             } // end of catch     
         } // end of Main
